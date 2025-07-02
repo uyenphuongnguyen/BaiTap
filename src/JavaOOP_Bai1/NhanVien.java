ab17c9b5e9e01bf78e101fd1280c9dbbcdcdde88
uyenphuongnguyen <106152305+uyenphuongnguyen@users.noreply.github.com>
2025-07-02 11:06:31 +0700

    Push bài tập OOP1

---
+++ src/JavaOOP_Bai1/NhanVien.java
@@ -0,0 +1,24 @@
+package JavaOOP_Bai1;
+
+import java.util.Date;
+
+public class NhanVien {
+
+    public String fullName;
+    public int age;
+    public Date birthDate;
+    public String gender;
+
+    public void createEmployee() {
+        System.out.println("Create Employee Success!");
+    }
+
+    public void updateEmployee() {
+        System.out.println("Update Employee Success!");
+    }
+
+    public void deleteEmployee() {
+        System.out.println("Delete Employee Success!");
+    }
+
+}
---
+++ src/JavaOOP_Bai1/ThongTin.java
@@ -0,0 +1,12 @@
+package JavaOOP_Bai1;
+
+public class ThongTin {
+
+    public static void main(String[] args) {
+        NhanVien nhanvien = new NhanVien();
+
+        System.out.println(nhanvien.fullName);
+        nhanvien.createEmployee();
+
+    }
+}
